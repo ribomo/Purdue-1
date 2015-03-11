b5d62248ff4fa7d7c576a463b2b750e91123ceae
Ribo Mo <ribomo@pal-nat184-101-101.itap.purdue.edu>
2015-03-11 16:32:29 -0400

    Add check function

--- CNIT-315/Lab4/Testers/Lab4_lib_test.h
+++ CNIT-315/Lab4/Testers/Lab4_lib_test.h
@@ -1,1 +1,35 @@
 //Lib for testers.
+#include <string.h>
+
+int checkSubstring(){
+text = "abd";
+if(*subString(text,3,1) != ""){
+return 1;
+}
+else if(*subString(text,-5,-3) != ""){
+return 1;
+}
+else if(*subString(text,1,4) != ""){
+return 1;
+}
+else if(*subString(text,-2,1) != ""){
+return 1;
+}
+else if(*subString(text,0,1) != "ab"){
+return 1;
+}
+text = "0123456789"
+else if(*subString(text,3,4) != "34"){
+return 1;
+}
+
+return 0;
+}
+
+int checkRemoveWord(){
+text = "abcdefg";
+RemoveWord(text,"bcdef");
+if(strcmp(text,"ag") != 0){
+return 1;
+}
+}

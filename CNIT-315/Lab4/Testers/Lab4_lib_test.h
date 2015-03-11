37a6aaf05945843818a987db45ebbd71c4812373
Ribo Mo <ribomo@pal-nat184-098-165.itap.purdue.edu>
2015-03-11 18:41:31 -0400

    Modify

--- CNIT-315/Lab4/Testers/Lab4_lib_test.h
+++ CNIT-315/Lab4/Testers/Lab4_lib_test.h
@@ -1,35 +1,39 @@
 //Lib for testers.
 #include <string.h>
 
+//For test
+char* subString(char*,int,int);
+void RemoveWord(char*,char*);
+
 int checkSubstring(){
-text = "abd";
-if(*subString(text,3,1) != ""){
-return 1;
-}
-else if(*subString(text,-5,-3) != ""){
-return 1;
-}
-else if(*subString(text,1,4) != ""){
-return 1;
-}
-else if(*subString(text,-2,1) != ""){
-return 1;
-}
-else if(*subString(text,0,1) != "ab"){
-return 1;
-}
-text = "0123456789"
-else if(*subString(text,3,4) != "34"){
-return 1;
-}
+	char text[200] = "abd";
+	if(strcmp(subString(text,3,1),"") != 0){
+		return 1;
+	}
+	else if(strcmp(subString(text,-5,-3),"") != 0){
+		return 1;
+	}
+	else if(strcmp(subString(text,1,4),"") != 0){
+		return 1;
+	}
+	else if(strcmp(subString(text,-2,1),"") != 0){
+		return 1;
+	}
+	else if(strcmp(subString(text,0,1),"ab") != 0){
+		return 1;
+	}
+	strcpy(text,"0123456789");
+	if(strcmp(subString(text,3,4),"34") != 0){
+		return 1;
+	}
 
 return 0;
 }
 
 int checkRemoveWord(){
-text = "abcdefg";
-RemoveWord(text,"bcdef");
-if(strcmp(text,"ag") != 0){
-return 1;
-}
+	char text[200] = "abcdefg";
+	RemoveWord(text,"bcdef");
+	if(strcmp(text,"ag") != 0){
+		return 1;
+	}
 }
